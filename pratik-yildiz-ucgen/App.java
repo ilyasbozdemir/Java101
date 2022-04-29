78db1f56d1aa558fb0c9b33375f9249fab337c18
İlyas Bozdemir <52322835+ilyasBozdemir@users.noreply.github.com>
2022-04-29 09:20:16 +0300

    Add files via upload

---
+++ pratik-yildiz-ucgen/App.java
@@ -0,0 +1,31 @@
+package hesap.makinesi;
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) {
+       
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sayı Giriniz :");
+        int n = input.nextInt();
+       
+        for (int i = 0; i <= n ; i++) {
+            for (int j = 0; j < (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i + 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+        for (int i = n; i >= 0 ; i--) 
+        {
+             for (int j = (n - i); j > 0; j--) {
+                 System.out.print(" ");
+            }
+             for (int k = (2 * i + 1); k >=1 ; k--) {
+                System.out.print("*");
+            }
+             System.out.println(" ");
+        }
+    }
+
+}
