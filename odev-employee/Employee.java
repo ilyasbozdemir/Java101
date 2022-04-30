26c19129b70c5abca341248b7510ab7648afbbb4
İlyas Bozdemir <52322835+ilyasBozdemir@users.noreply.github.com>
2022-04-30 19:40:03 +0300

    Add files via upload

---
+++ odev-employee/Employee.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package odev.employee;
+
+/**
+ *
+ * @author BozdemirHome
+ */
+public class Employee {
+    /*
+    name : Çalışanın adı ve soyadı
+    salary : Çalışanın maaşı
+    workHours : Haftalık çalışma saati
+    hireYear : İşe başlangıç yılı
+    */
+    /*
+    Employee(name,salary,workHours,hireYear) : Kurucu metot olup 4 parametre alacaktır.
+    tax() : Maaşa uygulanan vergiyi hesaplayacaktır.
+    Çalışanın maaşı 1000 TL'den az ise vergi uygulanmayacaktır.
+    Çalışanın maaşı 1000 TL'den fazla ise maaşının %3'ü kadar vergi uygulanacaktır.
+    bonus() : Eğer çalışan haftada 40 saatten fazla çalışmış ise fazladan çalıştığı her saat başına 30 TL olacak şekilde bonus ücretleri hesaplayacaktır.
+    raiseSalary() : Çalışanın işe başlangıç yılına göre maaş artışını hesaplayacaktır. Şuan ki yılı 2021 olarak alın.
+    Eğer çalışan 10 yıldan az bir süredir çalışıyorsa maaşına %5 zam yapılacaktır.
+    Eğer çalışan 9 yıldan fazla ve 20 yıldan az çalışıyorsa maaşına %10 zam yapılacaktır.
+    Eğer çalışan 19 yıldan fazla çalışıyorsa %15 zam yapılacaktır.
+    toString() : Çalışana ait bilgileri ekrana bastıracaktır.
+    
+    */
+    
+    /*
+    Adı : kemal
+    Maaşı : 2000.0
+    Çalışma Saati : 45
+    Başlangıç Yılı : 1985
+    Vergi : 60.0
+    Bonus : 150.0
+    Maaş Artışı : 300.0
+    Vergi ve Bonuslar ile birlikte maaş : 2090.0
+    Toplam Maaş : 2300.0
+    
+    */
+}
