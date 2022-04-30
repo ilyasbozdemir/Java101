9aee4d7f97ea67201f58866bc668fe0a9ad0e501
İlyas Bozdemir <52322835+ilyasBozdemir@users.noreply.github.com>
2022-04-30 18:27:19 +0300

    Add files via upload

---
+++ pratik-obs/Teacher.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pratik.obs;
+
+/**
+ *
+ * @author BozdemirHome
+ */
+public class Teacher {
+    String name;
+    String mpno;
+    String branch;
+
+    public Teacher(String name, String mpno, String branch) {
+        this.name = name;
+        this.mpno = mpno;
+        this.branch = branch;
+    }
+}
