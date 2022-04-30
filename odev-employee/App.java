ae997351b0cc4414adfaf08f2ce41c909228f37e
İlyas Bozdemir <52322835+ilyasBozdemir@users.noreply.github.com>
2022-04-30 20:49:20 +0300

    Add files via upload

--- odev-employee/App.java
+++ odev-employee/App.java
@@ -4,11 +4,16 @@
 package odev.employee;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
-
+ 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+
+        Employee employee = new Employee("Ali",1500,50,1980);   
+        employee.ToString();
+        
+        Employee employee2 = new Employee("Mehmet",1000,45,2010);
+        employee2.ToString();
+        
+        
+        
     }
 }
--- odev-employee/Employee.java
+++ odev-employee/Employee.java
@@ -1,26 +1,24 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package odev.employee;
 
 /**
- *
  * @author BozdemirHome
  */
 public class Employee {
-    /*
-    name : Çalışanın adı ve soyadı
-    salary : Çalışanın maaşı
-    workHours : Haftalık çalışma saati
-    hireYear : İşe başlangıç yılı
-    */
+  
+    String name; 
+    double salary;
+    int workHours;
+    int hireYear;
+    
     /*
     Employee(name,salary,workHours,hireYear) : Kurucu metot olup 4 parametre alacaktır.
     tax() : Maaşa uygulanan vergiyi hesaplayacaktır.
     Çalışanın maaşı 1000 TL'den az ise vergi uygulanmayacaktır.
     Çalışanın maaşı 1000 TL'den fazla ise maaşının %3'ü kadar vergi uygulanacaktır.
-    bonus() : Eğer çalışan haftada 40 saatten fazla çalışmış ise fazladan çalıştığı her saat başına 30 TL olacak şekilde bonus ücretleri hesaplayacaktır.
+    
+    bonus() : Eğer çalışan haftada 40 saatten fazla çalışmış ise fazladan çalıştığı 
+    her saat başına 30 TL olacak şekilde bonus ücretleri hesaplayacaktır.
     raiseSalary() : Çalışanın işe başlangıç yılına göre maaş artışını hesaplayacaktır. Şuan ki yılı 2021 olarak alın.
     Eğer çalışan 10 yıldan az bir süredir çalışıyorsa maaşına %5 zam yapılacaktır.
     Eğer çalışan 9 yıldan fazla ve 20 yıldan az çalışıyorsa maaşına %10 zam yapılacaktır.
@@ -28,17 +26,58 @@ public class Employee {
     toString() : Çalışana ait bilgileri ekrana bastıracaktır.
     
     */
+    public Employee(String name,double salary,int workHours,int hireYear){
+        this.name = name;
+        this.salary = salary;
+        this.workHours = workHours;
+        this.hireYear = hireYear;
+    }
+    public double tax(){
+         if(salary < 1000){
+            return 0;
+        }
+        else{
+            return this.salary * 0.03;
+        }
+    }
+    public double bonus(){
+       if (workHours >= 40){
+            return  ((workHours-40)*30);
+        }
+        else return 0;
+    }
+    public double raiseSalary(){
+        int nowYear=2021;
+        
+        int year= nowYear-this.hireYear;
+        
+        if(year<10){
+            return this.salary*0.05;
+        }
+        else if(year>9&&year<20){
+            return this.salary*0.1;
+        }
+        else return this.salary*0.15;
+    }
     
-    /*
-    Adı : kemal
-    Maaşı : 2000.0
-    Çalışma Saati : 45
-    Başlangıç Yılı : 1985
-    Vergi : 60.0
-    Bonus : 150.0
-    Maaş Artışı : 300.0
-    Vergi ve Bonuslar ile birlikte maaş : 2090.0
-    Toplam Maaş : 2300.0
     
-    */
+    public  void ToString(){
+    
+        double totalSalary=(this.salary-tax()+bonus()+raiseSalary());
+        double taxBonusSalary=(this.salary-tax()+bonus());
+        
+        System.out.println("----------------->>");
+        System.out.println("Adı : "+this.name );
+        System.out.println("Maaşı : "+this.salary ); 
+        System.out.println("Çalışma Saati : "+this.workHours ); 
+        System.out.println("Başlangıç Yılı : "+this.name );     
+        System.out.println("Vergi : "+this.tax() ); 
+        System.out.println("Bonus : "+this.bonus() );
+        System.out.println("Maaş Artışı  : " + this.raiseSalary() );
+        System.out.println("Vergi ve Bonuslar ile birlikte maaş : " + taxBonusSalary );
+        System.out.println("Toplam Maaş : "+ totalSalary );
+        System.out.println("----------------->>");
+        System.out.println();
+    }
+    
 }
